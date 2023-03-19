e2b47010a897e68c9534b400f40d1d3dc489cea6
tomqin <31796736+tomqingo@users.noreply.github.com>
2023-03-19 10:44:30 +0800

    Add files via upload

--- org/main.c
+++ org/main.c
@@ -4,7 +4,7 @@
 
 int FIFO_cache_put(int Newpageno, node_t **head_fifo, int cache_capacity);
 int LRU_cache_put(int Newpageno, node_t **head_lru, int cache_capacity);
-void DisplayResult(node_t *head);
+void DisplayResult(node_t *head, FILE *fp_out);
 
 int main(int argc, char *argv[])
 {
@@ -12,18 +12,22 @@ int main(int argc, char *argv[])
     printf("------------------------------------------------------------\n");
     printf("Page Replacement Simulated Program for CSCI 3150 (Hong Xu, Henry@CSE,CUHK)\n");
 
-    FILE *fp = NULL;
+    FILE *fp_in = NULL;
+    FILE *fp_out = NULL;
+
     int cache_capacity; //The size of FIFO cache and LRU cache
     int refer_total; //The total of elements in refernce row
 
-    fp = fopen(argv[2], "r");
-    fscanf(fp, "%d", &cache_capacity);
-    fscanf(fp, "%d", &refer_total);
+    fp_in = fopen(argv[2], "r");
+    fscanf(fp_in, "%d", &cache_capacity);
+    fscanf(fp_in, "%d", &refer_total);
 
     int pageno[refer_total]; //The reference row
 
     for(int i=0; i<refer_total; i++)
-        fscanf(fp, "%d", &pageno[i]);
+        fscanf(fp_in, "%d", &pageno[i]);
+
+    fclose(fp_in);
 
     //int pageno[12] = {4,3,2,1,4,3,5,4,3,2,1,5};
     //int cache_capacity = 4;
@@ -37,18 +41,24 @@ int main(int argc, char *argv[])
     int page_fault_flag_FIFO;
     int page_fault_flag_LRU;
 
+    fp_out = fopen(argv[4], "w+");
+
+
 
     for(int i=0; i<refer_total; i++)
     {
         page_fault_flag_FIFO = FIFO_cache_put(pageno[i], &head_fifo, cache_capacity);
         printf("After assessing %d, the elements in FIFO cache:", pageno[i]);
-        DisplayResult(head_fifo);
+        fprintf(fp_out, "After assessing %d, the elements in FIFO cache:", pageno[i]);
+        DisplayResult(head_fifo, fp_out);
         
         page_fault_flag_LRU = LRU_cache_put(pageno[i], &head_lru, cache_capacity);
         printf("the elements in LRU cache:");
-        DisplayResult(head_lru);
+        fprintf(fp_out, "the elements in LRU cache:");
+        DisplayResult(head_lru, fp_out);
 
         printf("\n");
+        fprintf(fp_out, "\n");
 
         if(page_fault_flag_FIFO)
             page_fault_time_FIFO++;
@@ -58,14 +68,16 @@ int main(int argc, char *argv[])
     }
 
     printf("Total times of missing in FIFO:%d\n", page_fault_time_FIFO);
-    printf("Total times of missing in LRU:%d\n", page_fault_time_LRU);    
+    fprintf(fp_out, "Total times of missing in FIFO:%d\n", page_fault_time_FIFO);
+    printf("Total times of missing in LRU:%d\n", page_fault_time_LRU);
+    fprintf(fp_out, "Total times of missing in LRU:%d\n", page_fault_time_LRU);  
 
     return 0;
 
 }
 
 
-void DisplayResult(node_t *head)
+void DisplayResult(node_t *head, FILE *fp_out)
 {
     node_t *curr;
     curr = head;
@@ -73,6 +85,7 @@ void DisplayResult(node_t *head)
     while(curr!=NULL)
     {
         printf("%d ", curr->page_no);
+        fprintf(fp_out, "%d ", curr->page_no);
         curr = curr->next;
     }
 }
